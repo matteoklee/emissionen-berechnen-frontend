cbd89cde6fbb5462a5612cdd50b27a6e5e1fb422
Matteo Kleemann <117347339+matteoklee@users.noreply.github.com>
2024-12-29 21:18:43 +0100

    steps layout

--- src/components/calculation/CalculationFormNew.vue
+++ src/components/calculation/CalculationFormNew.vue
@@ -3,13 +3,19 @@
       class="min-h-screen flex flex-col items-center max-w-screen-xl mx-auto text-black"
   >
     <div class="bg-white w-full">
-      <div class="flex flex-row items-center border divide-x">
-        <div class="h-full flex justify-center items-center px-6">
-          <X type="button" strokeWidth="1" />
-        </div>
+      <div class="flex lg:flex-row border h-full flex-col items-center lg:border lg:divide-x lg:divide-y-0 divide-y lg:h-[120px] lg:m-0 m-6">
 
-        <div v-for="step in steps" :key="step.id" class="h-full w-full flex justify-center items-center p-4">
-          <div v-if="step.status === 'Complete'" class="flex flex-row items-center justify-between w-full">
+        <div v-for="step in steps" :key="step.id" class="h-full w-full flex justify-center items-center">
+          <div v-if="step.status === 'cancel'" class="h-full flex justify-center items-center p-2">
+            <X type="button" strokeWidth="1" />
+          </div>
+          <div v-else-if="step.status === 'submit'" class="w-full h-full flex justify-center items-center inline-flex">
+            <Button class="w-full h-full rounded-none text-xl font-normal inline-flex items-center">
+              <Send  strokeWidth="2" class="" />
+              Berechnen
+            </Button>
+          </div>
+          <div v-else-if="step.status === 'Complete'" class="flex flex-row items-center justify-between w-full p-4">
             <div class="">
               <p class="text-sm text-green-500">{{ step.status }}</p>
               <p class="font-medium">{{ step.id }} {{ step.title }}</p>
@@ -19,7 +25,7 @@
               <Check strokeWidth="2" class="text-green-500" />
             </div>
           </div>
-          <div v-else class="flex flex-row items-center justify-between w-full">
+          <div v-else class="flex flex-row items-center justify-between w-full p-4">
             <div class="">
               <p class="text-sm">{{ step.status }}</p>
               <div class="">
@@ -29,13 +35,6 @@
               <p class="text-sm">{{ step.subtitle }}</p>
             </div>
           </div>
-        </div>
-
-        <div class="h-full w-full flex justify-center items-center">
-          <Button class="w-full h-full rounded-none text-xl font-normal inline-flex items-center">
-            <Send  strokeWidth="2" class="" />
-            Berechnen
-          </Button>
         </div>
       </div>
     </div>
@@ -52,6 +51,12 @@ export default {
   data() {
     return {
       steps: [
+        {
+          id: 0,
+          title: "Cancel",
+          subtitle: "Cancel",
+          status: "cancel",
+        },
         {
           id: 1,
           title: "Integration Details",
@@ -73,8 +78,14 @@ export default {
         {
           id: 4,
           title: "Data Exchange Jobs",
-          subtitle: "Connection Settings Received d d d Connection Settings Received",
+          subtitle: "Connection Settings Received",
           status: "Current",
+        },
+        {
+          id: 99,
+          title: "Submit",
+          subtitle: "Submit",
+          status: "submit",
         },
       ]
     }
