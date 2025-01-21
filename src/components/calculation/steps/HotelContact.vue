a9addb571a3cf01f616725239a8860706592dc85
Matteo Kleemann <117347339+matteoklee@users.noreply.github.com>
2025-01-21 10:26:03 +0100

    adding dummyData for hotelcontact and hoteldetails

--- src/components/calculation/steps/HotelContact.vue
+++ src/components/calculation/steps/HotelContact.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="px-8 py-4 w-full">
     <form @submit.prevent="onSubmit">
-      <div class="flex flex-col mb-2">
-        <Button type="button" variant="outline" size="" class="text-md py-6 bg-black text-white">
+      <div class="flex mb-2 space-x-4">
+        <Button type="button" variant="outline" size="" class="w-full text-md py-6 bg-black text-white" @click="setDummyData()">
           <Upload class="w-4 h-4 mr-2" />
-          Hotelkontakt übernehmen</Button
-        >
+          Hotelkontakt übernehmen
+        </Button>
+        <Button type="button" variant="outline" size="" class="w-full text-md py-6 bg-red-500 text-white" @click="reset()">
+          <Trash2 class="w-4 h-4 mr-2" />
+          Eingaben löschen
+        </Button>
       </div>
 
       <Separator label="oder manuell eingeben" class="my-6 bg-black" />
@@ -16,7 +20,7 @@
           <div class="w-1/2">
             <Label for="name" class="font-medium">Name</Label>
             <Input
-                v-model="name"
+                v-model="contactInfo.name"
                 @update:model-value="update('name', $event)"
                 id="name"
                 type="text"
@@ -27,7 +31,7 @@
           <div class="w-1/2">
             <Label for="jobTitle" class="font-medium">Job Title</Label>
             <Input
-                v-model="jobTitle"
+                v-model="contactInfo.jobTitle"
                 @update:model-value="update('jobTitle', $event)"
                 id="jobTitle"
                 type="text"
@@ -39,7 +43,7 @@
         <div class="w-full">
           <Label for="endOfReportingYear" class="font-medium">Ende des Meldejahres</Label>
           <Input
-              v-model="endOfReportingYear"
+              v-model="contactInfo.endOfReportingYear"
               @update:model-value="update('endOfReportingYear', $event)"
               id="endOfReportingYear"
               type="text"
@@ -55,7 +59,7 @@
           <div class="w-1/2">
             <Label for="hotelName" class="font-medium">Name des Hotels</Label>
             <Input
-                v-model="hotelName"
+                v-model="contactInfo.hotelName"
                 @update:model-value="update('hotelName', $event)"
                 id="hotelName"
                 type="text"
@@ -66,7 +70,7 @@
           <div class="w-1/2">
             <Label for="hotelGroupName" class="font-medium">Name der Hotelgruppe</Label>
             <Input
-                v-model="hotelGroupName"
+                v-model="contactInfo.hotelGroupName"
                 @update:model-value="update('hotelGroupName', $event)"
                 id="hotelGroupName"
                 type="text"
@@ -83,7 +87,7 @@
           <div class="w-1/2">
             <Label for="street" class="font-medium">Straße</Label>
             <Input
-                v-model="street"
+                v-model="contactInfo.address.street"
                 @update:model-value="updateAddress('street', $event)"
                 id="street"
                 type="text"
@@ -94,7 +98,7 @@
           <div class="w-1/2">
             <Label for="hnr" class="font-medium">Hausnummer</Label>
             <Input
-                v-model="hnr"
+                v-model="contactInfo.address.hnr"
                 @update:model-value="updateAddress('hnr', $event)"
                 id="hnr"
                 type="text"
@@ -107,7 +111,7 @@
           <div class="w-1/2">
             <Label for="country" class="font-medium">Land</Label>
             <Input
-                v-model="country"
+                v-model="contactInfo.address.country"
                 @update:model-value="updateAddress('country', $event)"
                 id="country"
                 type="text"
@@ -118,7 +122,7 @@
           <div class="w-1/2">
             <Label for="state" class="font-medium">Bundesland</Label>
             <Input
-                v-model="state"
+                v-model="contactInfo.address.state"
                 @update:model-value="updateAddress('state', $event)"
                 id="state"
                 type="text"
@@ -131,7 +135,7 @@
           <div class="w-1/2">
             <Label for="postcode" class="font-medium">PLZ</Label>
             <Input
-                v-model="postcode"
+                v-model="contactInfo.address.postcode"
                 @update:model-value="updateAddress('postcode', $event)"
                 id="postcode"
                 type="text"
@@ -142,7 +146,7 @@
           <div class="w-1/2">
             <Label for="city" class="font-medium">Stadt</Label>
             <Input
-                v-model="city"
+                v-model="contactInfo.address.city"
                 @update:model-value="updateAddress('city', $event)"
                 id="city"
                 type="text"
@@ -161,13 +165,13 @@
 import Input from "@/components/ui/input/Input.vue";
 import Label from "@/components/ui/label/Label.vue";
 import Button from "@/components/ui/button/Button.vue";
-import {Upload} from "lucide-vue-next";
+import {Upload, Trash2} from "lucide-vue-next";
 import Separator from "@/components/ui/separator/Separator.vue";
 import {useFootprintStore} from "@/stores/footprintStore.js";
 
 export default {
   name: "HotelContact",
-  components: {Separator, Button, Label, Input, Upload},
+  components: {Separator, Button, Label, Input, Upload, Trash2},
   setup() {
     const footprintStore = useFootprintStore();
     return {
@@ -176,6 +180,9 @@ export default {
   },
   data() {
     return {
+      contactInfo: this.footprintStore.formData.contactInfo,
+
+      /*
       name: this.footprintStore.formData.contactInfo.name,
       jobTitle: this.footprintStore.formData.contactInfo.jobTitle,
       endOfReportingYear: this.footprintStore.formData.contactInfo.endOfReportingYear,
@@ -188,6 +195,7 @@ export default {
       country: this.footprintStore.formData.contactInfo.address.country,
       state: this.footprintStore.formData.contactInfo.address.state,
       postcode: this.footprintStore.formData.contactInfo.address.postcode,
+       */
     }
   },
   methods: {
@@ -199,6 +207,45 @@ export default {
     },
     updateAddress(item, value) {
       this.footprintStore.formData.contactInfo.address[item] = value;
+    },
+    updateContactInfo() {
+      this.contactInfo = this.footprintStore.formData.contactInfo;
+    },
+    setDummyData() {
+      this.footprintStore.formData.contactInfo = {
+        name: "Maria Müller",
+        jobTitle: "Sustainability Manager",
+        endOfReportingYear: "2023-12-31",
+        hotelName: "Hotel am See",
+        hotelGroupName: "Seehotels",
+        address: {
+          street: "Seestraße",
+          hnr: "12",
+          city: "Rostock",
+          state: "Mecklenburg-Vorpommern",
+          postcode: "18147",
+          country: "Germany"
+        }
+      };
+      this.updateContactInfo();
+    },
+    reset() {
+      this.footprintStore.formData.contactInfo = {
+        name: '',
+        jobTitle: '',
+        endOfReportingYear: '',
+        hotelName: '',
+        hotelGroupName: '',
+        address: {
+          street: '',
+          hnr: '',
+          city: '',
+          state: '',
+          postcode: '',
+          country: '',
+        }
+      };
+      this.updateContactInfo();
     }
   }
 }
--- src/components/calculation/steps/HotelDetails.vue
+++ src/components/calculation/steps/HotelDetails.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="w-full px-8 py-4">
     <form @submit.prevent="onSubmit">
-      <div class="flex flex-col mb-2">
-        <Button type="button" variant="outline" class="text-md py-6 border bg-black text-white">
+      <div class="flex mb-2 space-x-4">
+        <Button type="button" variant="outline" size="" class="w-full text-md py-6 bg-black text-white" @click="setDummyData()">
           <Upload class="w-4 h-4 mr-2" />
-          Hoteldetails übernehmen</Button
-        >
+          Hotelkontakt übernehmen
+        </Button>
+        <Button type="button" variant="outline" size="" class="w-full text-md py-6 bg-red-500 text-white" @click="reset()">
+          <Trash2 class="w-4 h-4 mr-2" />
+          Eingaben löschen
+        </Button>
       </div>
 
       <Separator label="oder manuell eingeben" class="my-6 bg-black" />
@@ -14,7 +18,7 @@
         <div class="w-full">
           <Label for="guestRoomArea" class="font-medium">Total area of guest rooms and corridors</Label>
           <Input
-              v-model="guestRoomArea"
+              v-model="hotelDetails.guestRoomArea"
               @update:model-value="update('guestRoomArea', $event)"
               id="guestRoomArea"
               type="number"
@@ -25,7 +29,7 @@
         <div class="w-full">
           <Label for="meetingArea" class="font-medium">Total area of meeting facility space</Label>
           <Input
-              v-model="meetingArea"
+              v-model="hotelDetails.meetingArea"
               @update:model-value="update('meetingArea', $event)"
               id="meetingArea"
               type="number"
@@ -36,7 +40,7 @@
         <div class="w-full">
           <Label for="conditionedArea" class="font-medium">Total area of conditioned space*</Label>
           <Input
-              v-model="conditionedArea"
+              v-model="hotelDetails.conditionedArea"
               @update:model-value="update('conditionedArea', $event)"
               id="conditionedArea"
               type="number"
@@ -47,7 +51,7 @@
         <div class="w-full">
           <Label for="roomCount" class="font-medium">Total number of guest rooms</Label>
           <Input
-              v-model="roomCount"
+              v-model="hotelDetails.roomCount"
               @update:model-value="update('roomCount', $event)"
               id="roomCount"
               type="number"
@@ -58,7 +62,7 @@
         <div class="w-full">
           <Label for="totalOccupiedRooms" class="font-medium">Total number of occupied rooms* for reporting year</Label>
           <Input
-              v-model="totalOccupiedRooms"
+              v-model="hotelDetails.totalOccupiedRooms"
               @update:model-value="update('totalOccupiedRooms', $event)"
               id="totalOccupiedRooms"
               type="number"
@@ -75,7 +79,7 @@
 
 <script>
 import Button from "@/components/ui/button/Button.vue";
-import {Upload} from "lucide-vue-next";
+import {Upload, Trash2} from "lucide-vue-next";
 import Separator from "@/components/ui/separator/Separator.vue";
 import Label from "@/components/ui/label/Label.vue";
 import Input from "@/components/ui/input/Input.vue";
@@ -83,7 +87,7 @@ import {useFootprintStore} from "@/stores/footprintStore.js";
 
 export default {
   name: "HotelDetails",
-  components: {Input, Label, Separator, Button, Upload},
+  components: {Input, Label, Separator, Button, Upload, Trash2},
   setup() {
     const footprintStore = useFootprintStore();
     return {
@@ -92,17 +96,45 @@ export default {
   },
   data() {
     return {
+      hotelDetails: this.footprintStore.formData.hotelDetails,
+      /*
       guestRoomArea: this.footprintStore.formData.hotelDetails.guestRoomArea,
       meetingArea: this.footprintStore.formData.hotelDetails.meetingArea,
       conditionedArea: this.footprintStore.formData.hotelDetails.conditionedArea,
       roomCount: this.footprintStore.formData.hotelDetails.roomCount,
       totalOccupiedRooms: this.footprintStore.formData.hotelDetails.totalOccupiedRooms,
+       */
     }
   },
   methods: {
     update(item, value) {
       this.footprintStore.formData.hotelDetails[item] = value;
     },
+    setDummyData() {
+      this.footprintStore.formData.hotelDetails = {
+        guestRoomArea: 3500.0,
+        meetingArea: 450.0,
+        conditionedArea: 4200.0,
+        roomCount: 120,
+        totalOccupiedRooms: 35040,
+        privateSpacePercentage: 0.75
+      };
+      this.updateHotelDetails();
+    },
+    reset() {
+      this.footprintStore.formData.hotelDetails = {
+        guestRoomArea: 0,
+        meetingArea: 0,
+        conditionedArea: 0,
+        roomCount: 0,
+        totalOccupiedRooms: 0,
+        privateSpacePercentage: 0
+      };
+      this.updateHotelDetails();
+    },
+    updateHotelDetails() {
+      this.hotelDetails = this.footprintStore.formData.hotelDetails;
+    }
   }
 }
 </script>
